5f9951febb08c3059549e26cce011a37f0cdee21
Nickolay Babenko <babenko.nickolay@gmail.com>
2024-10-02 10:23:42 +0200

    no message

--- setup_matrix.sh
+++ setup_matrix.sh
@@ -175,7 +175,9 @@ nginx -t && service nginx restart
 echo -e "To: $ALERT_EMAIL\nFrom: no-reply@$SYNAPSE_SERVER_DOMAIN_NAME\nSubject: Matrix Synapse Server Setup Completed\n\nMatrix Synapse Server Setup Completed Successfully." | ssmtp $ALERT_EMAIL
 
 # Add a cron job to run backup_matrix.sh every night at 2:00 AM CET
-(crontab -l 2>/dev/null | grep -v "backup_matrix.sh"; echo "0 2 * * * echo \"Backup started at \$(date)\" >> $LOG_FILE; /bin/bash /matrix-synapse/backup_matrix.sh >> $LOG_FILE 2>&1; echo \"Backup finished at \$(date)\" >> $LOG_FILE") | crontab - || echo "Failed to add cron job"
+(crontab -l 2>/dev/null | grep -v "backup_matrix.sh"; echo "0 1 * * * echo \"Backup started at \$(date)\" >> $LOG_FILE; /bin/bash /matrix-synapse/backup_matrix.sh >> $LOG_FILE 2>&1; echo \"Backup finished at \$(date)\" >> $LOG_FILE") | crontab - || echo "Failed to add cron job"
+
+service cron start
 
 echo "Cron job added. The backup will run every night at 2:00 AM CET."
 
